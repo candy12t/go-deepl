5f4a50e394a64fb8d3744163317b4b6f36dd7400
Takumi Kanada <candy12t@gmail.com>
2024-01-12 23:52:06 +0900

    Add: get glossary language pairs API

--- languages.go
+++ languages.go
@@ -34,3 +34,23 @@ func (c *Client) GetLanguages(ctx context.Context, langType langType) ([]Languag
 	}
 	return languages, nil
 }
+
+type GlossaryLanguagePairs struct {
+	SupportedLanguages []struct {
+		SourceLang string `json:"source_lang"`
+		TargetLang string `json:"target_lang"`
+	} `json:"supported_languages"`
+}
+
+func (c *Client) GetGlossaryLanguagesPairs(ctx context.Context) (*GlossaryLanguagePairs, error) {
+	req, err := c.NewRequest(ctx, http.MethodGet, "/glossary-language-pairs", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	languages := new(GlossaryLanguagePairs)
+	if _, err := c.Do(req, languages); err != nil {
+		return nil, err
+	}
+	return languages, nil
+}
